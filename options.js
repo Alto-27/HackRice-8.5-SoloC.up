3a64aa9b1f42d3f113aad85885862d3f0b188373
Elena Margolin <elenamargolin@elenas-mbp.dyndns.rice.edu>
2019-03-01 22:35:56 -0600

    first stuff

---
+++ options.js
@@ -0,0 +1,15 @@
+  let page = document.getElementById('buttonDiv');
+  const kButtonColors = ['#000000', '#b7dbff', '#ffeda0', '#ffcef9'];
+  function constructOptions(kButtonColors) {
+    for (let item of kButtonColors) {
+      let button = document.createElement('button');
+      button.style.backgroundColor = item;
+      button.addEventListener('click', function() {
+        chrome.storage.sync.set({color: item}, function() {
+          console.log('color is ' + item);
+        })
+      });
+      page.appendChild(button);
+    }
+  }
+  constructOptions(kButtonColors);
