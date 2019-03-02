242585a4274ca4c30091b56ee62b0fab94321bd9
Samsung27 <chromium.ramirez@gmail.com>
2019-03-02 11:51:43 -0600

    Added event listener instead of inline javascript

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 // This function will take in a number and a unit to convert from
 // It will change the conversion factor according the given unit
 // The function will output the number of units given into the respective amount of solo cups
-function convert() {
+document.getElementById("conversion").addEventListener("click", function() {
     let num = parseFloat(document.getElementById('myText').value);
     let unit = document.getElementById('mySelect').value;
     let convertFactor = 1.0;
@@ -31,4 +31,4 @@ function convert() {
             break;
     }
     document.getElementById('myOutput').value = num * convertFactor;
-}
+});
